ee3a078ec819b214650e7df5ebedda801576796d
Stephanie J. Spielman <spielman@rowan.edu>
2020-02-05 10:36:25 -0500

    fixed bugs in parse_aic that sjs probably put there

--- scripts/parse_aic.py
+++ scripts/parse_aic.py
@@ -1,43 +1,50 @@
+### SJS and FJP. Parse all IQTREE files for information theoretic criteria scores.
+
 import os
 import sys
-import csv
 
 TEXT_AIC  = "Akaike information criterion (AIC) score:"
 TEXT_AICC = "Corrected Akaike information criterion (AICc) score:"
 TEXT_BIC  = "Bayesian information criterion (BIC) score:"
 
 def find_score(text, line):
+    score = None
     if text in line: 
-		score = line.split(" ")[-1].strip()
+        score = line.split(" ")[-1].strip()
     return score
-    
-    
-input_path = "../branch_length_inference/"
+
+
+assert(len(sys.argv) == 2), "\nUSAGE: python3 parse_aic.py <path_to_input_files>"
+input_path = sys.argv[1]
 output_path = "../results/"
+
 
 
 iq_files = os.listdir(input_path)
 
 out_file = open(output_path + "parsed_aic.csv", "w")
-header = "sites,bls,models,aic,aicc,bic\n"
+header = "site,bl,model,AIC,AICc,BIC\n"
 out_file.write(header)
 
 for file in iq_files:
     if not file.endswith(".iqtree"):
         continue
     open_iqfile = open(input_path + file, "r")
-	file_info = file.split("_")
-	
-	for line in open_iqfile:
-	    aic_score  = find_score(TEXT_AIC, line)
-	    aicc_score = find_score(TEXT_AICC, line)
-	    bic_score  = find_score(TEXT_BIC, line)
+    file_info = file.replace("bl","").replace("site","").replace(".iqtree","").split("_")
+    aic_score  = None
+    aicc_score = None
+    bic_score  = None
+    for line in open_iqfile:
+        if aic_score is None:
+            aic_score  = find_score(TEXT_AIC, line)
+        if aicc_score is None:
+            aicc_score = find_score(TEXT_AICC, line)
+        if bic_score is None:
+            bic_score  = find_score(TEXT_BIC, line)
 
-        file_info.append(aic_score + "," + aicc_score + "," + bic_score)
-		#print(",".join(split_files)+"\n")
-		out_file.write(",".join(file_info) + "\n")
-
-	open_iqfile.close()
+    file_info.append(aic_score + "," + aicc_score + "," + bic_score)
+    out_file.write(",".join(file_info) + "\n")
+    open_iqfile.close()
 
 out_file.close()
 
