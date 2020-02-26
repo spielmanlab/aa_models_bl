659e005457224ec983f068c7655aafeed9814311
Frankie Picone <piconef8@students.rowan.edu>
2020-02-25 19:04:33 -0500

    everything works just need to have entropy score print out correctly

--- scripts/app.R
+++ scripts/app.R
@@ -1,13 +1,14 @@
 library(shiny)
 library(tidyverse)
-
 library(shinythemes)
 
 
 
 input_file <- "../results/final_tibble.csv"
 final_tibble <- read_csv(input_file)
-
+print(final_tibble)
+#entropy_value <- pull(final_tibble, entropy)
+#print(entropy_value)
 # Define UI for application that draws a scatterplot
 ui <- fluidPage(theme =shinytheme("darkly"),
     
@@ -24,23 +25,35 @@ ui <- fluidPage(theme =shinytheme("darkly"),
 
         # Show a plot of the generated final tibble
         mainPanel(
-           plotOutput("bl_plot")
+           plotOutput("bl_plot"),
+           br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),br(),
+           textOutput("entropy_score")
         )
     )
 )
 
 # Define server logic required to draw a scatterplot
-server <- function(input, output) {
-    output$bl_plot <- renderPlot(height = 600, width = 1250,{
+server <- function(input, output) {   
+    
+    output$entropy_score    <- renderText("Entropy for this site is ")
+    output$bl_plot          <- renderPlot(height = 600, width = 1250,{
+   
+   
+    
+        
        
         # define the dataframe
         #add modeled slope bias from results
         #make the app look nicer
         #add slopes to each plot x=1 and y=1
         #push all of this over to git
-        #npsite focus on entropy and print out under each plot the entropy use pull entropy and text output
-        final_tibble %>%
-        dplyr::filter(site == input$site) %>% 
+        #  focus on entropy and print out under each plot the entropy use pull entropy and text output
+        
+        #entropy_value <- pull(final_tibble, entropy)
+        #print(entropy_value)
+        final_tibble %>% 
+        dplyr::filter(site == input$site) %>%
+   
         ggplot(aes(x = persite_count, y = branch_length, color = model)) +
             geom_point()  + 
             geom_smooth(method = "lm") +  #, se = FALSE) +
@@ -50,10 +63,10 @@ server <- function(input, output) {
             labs(title = "Count vs Branch Length", x = "Persite Count", y = "Branch Length") +
             theme_bw(base_size = 18) +
             theme(plot.background = element_rect(fill = "white"),
-                  legend.position = "none", 
-                  strip.background = element_rect(color = "black", fill = "white")) +
-            geom_text( x = 0.5, y = 3, aes(label = bias),color = "black")
-        
+                legend.position = "none", 
+                strip.background = element_rect(color = "black", fill = "white")) +
+            geom_text( x = 0.6, y = 3, aes(label = bias),color = "black") 
+         
     })
 }
 
