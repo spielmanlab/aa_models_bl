9957c4fe0ad7952d5009b7bee97d772dd1efb796
Stephanie J. Spielman <spielman@rowan.edu>
2021-01-28 09:38:14 -0500

    script ready for LOCAL running

--- scripts/infer_empirical_branch_lengths.py
+++ scripts/infer_empirical_branch_lengths.py
@@ -13,26 +13,51 @@ threads      = sys.argv[3]
 
 if dataset_type == "mammal":
     treefile = path_to_data + "mammal.tre"
-        
-q_options = ["WAG", "JTT", "LG", "FLU", "JC"]
+
+outpath = path_to_data + "../" + dataset_type + "_optimized_bl/"
+   
+
+q_options = ["WAG", "JTT", "LG", "FLU", "Poisson"]
 g_options = ["+F", "+F+G"]
 
 fastas = [x for x in os.listdir(path_to_data) if x.endswith("fasta")]
 
+x=1
 for fasta in fastas:
     fasfile = path_to_data + fasta
     if dataset_type != "mammal":
         treefile = fasfile.replace(".fasta", ".tre")
-        
+    print(x) ## 9861 total
+    x += 1
     for matrix in q_options:
         for gamma in g_options:
             model = matrix + gamma
             # --tree-fix is borked, filed an issue: https://github.com/iqtree/iqtree2/issues/17
-            cmd = "iqtree2 -T " + threads + " -s " + fasfile + " -t " + treefile + " --tree-fix --redo"
-            print(cmd) 
+            # used -te instead for now ^^
+            #cmd = "iqtree2 -T " + threads + " -s " + fasfile + " -t " + treefile + " --tree-fix --redo"
+            cmd = "iqtree2 -T " + threads + " -s " + fasfile + " -m " + model + " -te " + treefile + " --redo --quiet"
+            #print(cmd) 
             cmdcode = os.system(cmd)
             assert(cmdcode == 0)   
-            #assert 1==3
-            # WRITE THE REST PLEASE
+            
+            # Move files to final resting place 
+            os.system("rm " + fasfile + "*gz " + fasfile + ".iqtree" )
+            os.system("mv " + fasfile + ".* " + outpath)
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
             
         
