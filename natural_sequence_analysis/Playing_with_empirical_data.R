111232d87520b5a83e33acea054ad3c1c7a4e435
Jake Mihalecz <jakemihalecz@gmail.com>
2021-03-02 15:54:05 -0500

    Cleaned the script up quite a bit and added functions for the modeling and graphing

--- natural_sequence_analysis/Playing_with_empirical_data.R
+++ natural_sequence_analysis/Playing_with_empirical_data.R
@@ -1,82 +1,52 @@
-# Load libraries ----------------------------
+# Load libraries -------------------------------------
 library(tidyverse)
-library(lme4) # random effects lm
+library(lme4) # for random effects linear model
 library(cowplot)
 
-#source("utils.R") # remove comment when ready!
+source("Utils.R")
 
 # Define important variables ----------------------------
 conditions_per_dataset <- 10  # 5 models * 2 ASRV = 10
 
 
-# Load datasets -----------------------------
+# Load datasets -------------------------------------
 
 # relative path! goal for all scripts = MEANT to run from directory where script lives 
-path_to_data <- "./" 
+#for some reason the code will not work with the relative path...even after checking to ensure I was in the current directory...
+path_to_data <- "/Users/jakemihalecz/Junior_year/Research/aa_models_bl/natural_sequence_analysis" 
+
 
 birds_file <- file.path(path_to_data, "bird_empirical_branch_lengths.csv")
 mammals_file <- file.path(path_to_data, "mammal_empirical_branch_lengths.csv")
 enzymes_file <- file.path(path_to_data, "enzyme_empirical_branch_lengths.csv")
 
 
-# optional strategy for saving time/sanity
-#if (!(exists("birds"))) {
-#  birds<- read_csv(birds_file)
-#} 
-
 mammals <- read_csv(mammals_file)
 enzymes <- read_csv(enzymes_file)
 birds   <- read_csv(birds_file)
 
-# Wrangle into treelengths tibbles ------------------------
+# Wrangle into branch lengths tibbles using functions ------------------------------
 
-
-
-
-# for reference:
-#add <- function(a,b){
-#  a+b
-#}
-
-
-summarize_branch_lengths <- function(input_df) 
-{
-  input_df %>%
-    group_by(model, ASRV, id)%>%
-    summarize(treelength = sum(branch_length),
-              mean_bl    = mean(branch_length),
-              median_bl  = median(branch_length),
-              sd_bl      = sd(branch_length),
-              cov        = sd_bl / mean_bl, 
-              min_bl     = min(branch_length),
-              max_bl     = max(branch_length)) %>%
-    ungroup()
-}
 
 bird_branch_lengths <- summarize_branch_lengths(birds)
-mammals_branch_lengths <- summarize_branch_lengths(mammals)
-enzymes_branch_lengths <- summarize_branch_lengths(enzymes)
+mammal_branch_lengths <- summarize_branch_lengths(mammals)
+enzyme_branch_lengths <- summarize_branch_lengths(enzymes)
 
 
 
-# Begin exploring -----------------------------------------
-birds%>%
+# Begin exploring (no specific goal for this section) -----------------------------------------------------------------
+birds%>% 
   filter(id==100, model== "WAG", ASRV=="FALSE")%>%
   summarize(treelength=sum(branch_length)) #checking one individual treelength
 
-#### Assertions 
-bird_treelengths %>% 
+#### Practice Assertions 
+bird_branch_lengths %>% 
   count(id) %>% 
   filter(n != conditions_per_dataset) %>%
   nrow() -> number_bad
 stopifnot( number_bad == 0 ) # if this isn't true, STOP!!!
 # inside parentheses put what SHOULD BE TRUE
 
-bird_treelengths%>%
-  group_by(model, ASRV)%>%
-  summarize(avg_tree_length= mean(treelength))%>%
-  ggplot(aes(x=model, y=avg_tree_length, fill=model))+
-  geom_col() #shows which model gives the highest avg tree length for the bird data (FLU)
 
 mammals %>%
   group_by(model, ASRV, id)%>%
@@ -84,26 +54,7 @@ mammals %>%
   ungroup() %>%
   ggplot(aes(x=model, y=treelength, fill=model))+ 
     geom_violin() + stat_summary() + 
-    facet_wrap(vars(ASRV), nrow=2)
-
-
-# similar to above but with something more numeric-y
-bird_treelengths%>%
-  ggplot(aes(x=model, y=treelength, color=model))+ 
-    geom_jitter(width = 0.2) + 
-    facet_wrap(vars(ASRV), nrow=2)
-
-
-mammals%>%
-  group_by(model, ASRV, id)%>%
-  summarize(treelength=sum(branch_length)) %>%
-  ungroup() ->mammal_treelengths # all treelengths for the mammal data
-
-mammal_treelengths%>%
-  group_by(model)%>%
-  summarize(avg_tree_length= mean(treelength))%>%
-  ggplot(aes(x=model, y=avg_tree_length, fill=model))+
-  geom_col() #FLU once again gives highest avg treelength amongst the models!
+    facet_wrap(vars(ASRV), nrow=2) #quick plot to visualize treelengths of mammal data across models with +G/-G
 
 
 mammals%>%
@@ -124,22 +75,6 @@ birds%>%
   group_by(ASRV)%>%
   summarize(avg_branch_length=mean(branch_length)) #as expected, when gamma is present, the branch length is higher because there can be different evolutionary rates between sites in models that include gamma
 
-birds%>%
-  filter(id==10004, model=="FLU", ASRV=="FALSE")->single_bird_tree
-
-  
-  single_bird_tree%>%
-    summarize(tree_length=sum(branch_length)) #find the treelength of a single tree
-
-birds%>%
-  group_by(id)%>%
- summarize(sum_of_all_branchlengths=sum(branch_length))%>%
-  arrange(desc(sum_of_all_branchlengths)) #this (I think)is a very crude way to show which genes are more conserved than others where larger branch lengths suggest higher tolerance to change. Interesting that some literally come out to be 0
-
-birds%>%
-  group_by(id, ASRV)%>%
-  summarize(avg_branch_length=mean(branch_length)) #just a quick way to view the differences between +G and -G for each gene reguardless of model.
-  
 
 enzymes%>%
   group_by(id, model, ASRV)%>%
@@ -149,143 +84,55 @@ enzymes%>%
   geom_col()+
   theme_classic() #visualizing the different model outputs with G for each model for a specific enzyme
 
-
-  
-
+#########################################################################################################
 
 
 
 
 
-# Mammals modeling-----------------------------------------
-
-lm(treelength ~ model + ASRV, data = mammal_treelengths) %>% 
-  aov() %>%
-  TukeyHSD()
-
-############################################################
-#################### Function me!!
-#now try to get the ASRV in the Tukey
-mammal_treelengths%>%
-  mutate(ASRV_modified= if_else(ASRV==TRUE, "Yes", "No"))-> mammal_treelengths_ASRV_modified
-
-
-lm(treelength ~ model + ASRV_modified, data = mammal_treelengths_ASRV_modified) %>% 
-  aov() %>%
-  TukeyHSD() -> result
-#################################################################
-#result$model %>% as_tibble(rownames = "comparison")
-#result$ASRV_modified %>% as_tibble(rownames = "comparison")
-
-#now with random effects
-lme4::lmer(treelength ~ model + ASRV + (1|id), data = mammal_treelengths) 
 
 
 
 
 
-# birds modeling--------------------------------------------
-
-
-lm(treelength ~ model + ASRV, data = bird_treelengths) %>% 
-  aov() %>%
-  TukeyHSD()
 
 
 
-#Now with ASRV
-bird_treelengths%>%
-  mutate(ASRV_modified= if_else(ASRV=="TRUE", "Yes", "No"))-> bird_treelengths_ASRV_modified
+#  Modeling with functions ------------------------------------------------------------------------
+#tried to add the ASRV coulumn and perform the linear model all in one function but R yelled at me
+
+#first, function to add the ASRV as a character into each branch length data set.
 
 
-lm(treelength ~ model + ASRV_modified, data = bird_treelengths_ASRV_modified) %>% 
-  aov() %>%
-  TukeyHSD()
-
-
-#now with random effects
-
-lme4::lmer(treelength ~ model + ASRV + (1|id), data = bird_treelengths) 
-
-
-#enzymes modeling-------------------------------------
-
-lm(treelength ~ model + ASRV, data = enzyme_treelengths) %>% 
-  aov() %>%
-  TukeyHSD()
-
-
-#now with ASRV
-enzyme_treelengths%>%
-  mutate(ASRV_modified= if_else(ASRV=="TRUE", "Yes", "No"))-> enzyme_treelengths_ASRV_modified
-
-lm(treelength ~ model + ASRV_modified, data = enzyme_treelengths_ASRV_modified) %>% 
-  aov() %>%
-  TukeyHSD()
-
-
-#now with random effects
-lme4::lmer(treelength ~ model + ASRV + (1|id), data = enzyme_treelengths) 
-
-# mess around with making some figures----------------------------------------------------------------------------
-
-mammal_treelengths%>%
-  pivot_wider(names_from = model, values_from = treelength)%>%
-  pivot_longer(cols=c("FLU", "JTT" , "Poisson", "WAG"), 
-               names_to = "other_models", 
-               values_to = "treelength")->mammal_treelengths_LG_pivot 
-
-#this wrangling will allow for a direct comparison of LG treelengths vs all other models!!!
-
-ggplot((mammal_treelengths_LG_pivot), aes(x=LG, y=treelength))+
-  # goal: 2 rows, 4 columns
-  facet_grid(other_models ~ ASRV), scales="free")+
-  geom_point()+
-  theme_classic()+
-  geom_smooth(method = "lm")+
-  geom_abline(color = "red") + 
-  cowplot::panel_border() + 
-  theme(strip.background =element_rect(fill="cornflowerblue"))+
-  labs(title= "LG vs. Other model's Mammal Treelengths", x="Treelength obtained from LG", y="Treelength obtained from other models")
-
-# now do the exact same thing for the other models!!!
-
-bird_treelengths%>%
-  pivot_wider(names_from = model, values_from = treelength)%>%
-  pivot_longer(cols=c("FLU", "JTT" , "Poisson", "WAG"), 
-               names_to = "other_models", 
-               values_to = "treelength")->bird_treelengths_LG_pivot 
-
-ggplot((bird_treelengths_LG_pivot), aes(x=LG, y=treelength))+
-  facet_wrap(vars(other_models))+
-  geom_point()+
-  theme_classic()+
-  geom_smooth(method = "lm")+
-  theme(strip.background =element_rect(fill="cornflowerblue"))+
-  labs(title= "LG vs. Other model's Bird Treelengths", x="Treelength obtained from LG", y="Treelength obtained from other models")
-``
+add_asrv_function(mammal_branch_lengths)->mammal_branch_lengths_ASRV_modified
+add_asrv_function(bird_branch_lengths)->bird_branch_lengths_ASRV_modified
+add_asrv_function(enzyme_branch_lengths)->enzyme_branch_lengths_ASRV_modified
 
 
 
-enzyme_treelengths%>%
-  pivot_wider(names_from = model, values_from = treelength)%>%
-  pivot_longer(cols=c("FLU", "JTT" , "Poisson", "WAG"), 
-               names_to = "other_models", 
-               values_to = "treelength")->enzyme_treelengths_LG_pivot 
+
+#next, build models
 
 
-ggplot((enzyme_treelengths_LG_pivot), aes(x=LG, y=treelength))+
-  facet_wrap(vars(other_models))+
-  geom_point()+
-  theme_classic()+
-  geom_smooth(method = "lm")+
-  theme(strip.background =element_rect(fill="cornflowerblue"))+
-  labs(title= "LG vs. Other model's Enzyme Treelengths", x="Treelength obtained from LG", y="Treelength obtained from other models")
+mammal_linear_model<- function_for_modeling(mammal_branch_lengths_ASRV_modified)
+bird_linear_model<- function_for_modeling(bird_branch_lengths_ASRV_modified)
+enzyme_linear_model<- function_for_modeling(enzyme_branch_lengths_ASRV_modified)
+
+
+#now, make the model output a little nicer on the eyes
+
+function_for_model_outputs(mammal_linear_model)->nice_output_mammal_model
+function_for_model_outputs(bird_linear_model)->nice_output_bird_model
+function_for_model_outputs(enzyme_linear_model)->nice_output_enzyme_model
 
 
 
-  
-  
+#Making figures with functions--------------------------------------------------------------
 
-  
+function_for_graphing(mammal_branch_lengths)->mammal_treelength_figure
+function_for_graphing(bird_branch_lengths)->bird_treelength_figure
+function_for_graphing(enzyme_branch_lengths)->enzyme_treelength_figure
+
+
+
   
