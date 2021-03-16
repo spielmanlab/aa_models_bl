d08bcad25666984adb6c6d0f8fc55d8d6ee0fc06
Jake Mihalecz <jakemihalecz@gmail.com>
2021-03-16 16:10:47 -0400

    Created a linear modeling function for specidic columns from the branchlength datasets using the double-curly method. Also looked at how the plotting function works and cleaned the script a little.

--- natural_sequence_analysis/Playing_with_empirical_data.R
+++ natural_sequence_analysis/Playing_with_empirical_data.R
@@ -1,15 +1,15 @@
-# Load libraries -------------------------------------
+# Load libraries ------------------------------------------------------------------------------------------------
 library(tidyverse)
 library(lme4) # for random effects linear model
 library(cowplot)
 
 source("utils.R")
 
-# Define important variables ----------------------------
+# Define important variables ------------------------------------------------------------------------------------
 conditions_per_dataset <- 10  # 5 models * 2 ASRV = 10
 
 
-# Load datasets -------------------------------------
+# Load datasets --------------------------------------------------------------------------------------------------
 
 # relative path! goal for all scripts = MEANT to run from directory where script lives 
 path_to_data <- "./"
@@ -24,7 +24,7 @@ mammals <- read_csv(mammals_file)
 enzymes <- read_csv(enzymes_file)
 birds   <- read_csv(birds_file)
 
-# Wrangle into branch lengths tibbles using functions ------------------------------
+# Wrangle into branch lengths tibbles using functions ------------------------------------------------------------
 
 
 bird_branch_lengths <- summarize_branch_lengths(birds)
@@ -32,84 +32,37 @@ mammal_branch_lengths <- summarize_branch_lengths(mammals)
 enzyme_branch_lengths <- summarize_branch_lengths(enzymes)
 
 
-
-# Begin exploring (no specific goal for this section) -----------------------------------------------------------------
-birds%>% 
-  filter(id==100, model== "WAG", ASRV=="FALSE")%>%
-  summarize(treelength=sum(branch_length)) #checking one individual treelength
-
-#### Practice Assertions 
-bird_branch_lengths %>% 
-  count(id) %>% 
-  filter(n != conditions_per_dataset) %>%
-  nrow() -> number_bad
-stopifnot( number_bad == 0 ) # if this isn't true, STOP!!!
-# inside parentheses put what SHOULD BE TRUE
-
-
-mammals %>%
-  group_by(model, ASRV, id)%>%
-  summarize(treelength=sum(branch_length)) %>%
-  ungroup() %>%
-  ggplot(aes(x=model, y=treelength, fill=model))+ 
-    geom_violin() + stat_summary() + 
-    facet_wrap(vars(ASRV), nrow=2) #quick plot to visualize treelengths of mammal data across models with +G/-G
-
-
-mammals%>%
-  group_by(id)%>%
-  count(id) #looks like there are about 10,000 unique genes in the mammals data
-
-
-birds%>%
-  group_by(id)%>%
-  count(id)%>%
-  arrange(id)#and over 11,000 bird genes
-
-enzymes%>%
-  group_by(id)%>%
-  count(id) #and about 500 bacterial enzyme-coding genes    
-
-birds%>%
-  group_by(ASRV)%>%
-  summarize(avg_branch_length=mean(branch_length)) #as expected, when gamma is present, the branch length is higher because there can be different evolutionary rates between sites in models that include gamma
-
-
-enzymes%>%
-  group_by(id, model, ASRV)%>%
-  summarize(treelength=sum(branch_length))%>%
-  filter(id=="13PK_A", ASRV=="TRUE")%>%
-  ggplot(aes(x=model, y=treelength))+
-  geom_col()+
-  theme_classic() #visualizing the different model outputs with G for each model for a specific enzyme
-
-#########################################################################################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-# DEPRECATED:  How does model+ASRV affect treelength? ----------------------------------------------------------------
+# DEPRECATED:  How does model+ASRV affect treelength? -----------------------------------------------------------------------------------------------------------------
 #fit_treelength_model_ASRV(mammal_branch_lengths) -> fitted_mammals_treelengths
 #fit_treelength_model_ASRV(bird_branch_lengths)   -> fitted_bird_treelengths
 #fit_treelength_model_ASRV(enzyme_branch_lengths) -> fitted_enzyme_treelengths
 
 
 
-# DEPRECATED: Scatterplot treelengths among models-----------------------------------
+# DEPRECATED: Scatterplot treelengths among models----------------------------------------------------------------
 #plot_compare_treelengths(mammal_branch_lengths) -> mammal_treelength_figure
 #plot_compare_treelengths(enzyme_branch_lengths) -> enzyme_treelength_figure
 #bird_branch_lengths %>%
 #  filter(treelength < 5) %>%
 #  plot_compare_treelengths() -> bird_treelength_figure
 
+
+
+
+#How does model+ASRV affect any of the parameters in the branchlength dataframes?---------------------------------
+
+#note:you have to use the money sign to specify the column in the argument:
+#linear_model_function_with_curlies<-function(input_branchlength_df, $dependent_variable_column)
+
+linear_model_function_with_curlies(enzyme_branch_lengths, enzyme_branch_lengths$mean_bl)
+linear_model_function_with_curlies(enzyme_branch_lengths, enzyme_branch_lengths$max_bl)
+linear_model_function_with_curlies(enzyme_branch_lengths, enzyme_branch_lengths$treelength)
+
+
+#Now, lets plot with the plotting function
+#plot_compare_function <- function(input_df, column_to_plot, plot_title, x_label, y_label)
+#Note: do not have to use $ in the argument for this function...
+
+plot_compare_function(enzyme_branch_lengths, mean_bl, "Mean bl of Other Models vs. Poisson", "Poisson bl", "Other model bl")
 
   
--- natural_sequence_analysis/utils.R
+++ natural_sequence_analysis/utils.R
@@ -14,34 +14,64 @@ summarize_branch_lengths <- function(input_df)
     ungroup()
 }
 
-# This function will take a dataframe of treelengths, protein model, ASRV and lm treelength
-fit_model_ASRV <- function(input_df, response_variable)
-{
-  input_df %>%
-    mutate(ASRV_modified= if_else(ASRV==TRUE, "Yes", "No")) -> df_for_modeling
+#################################################################
+
+
+
+
+
+# This function will take a dataframe of branchlengths and a response variable and output is the lm as a tibble
+
+#fit_model_ASRV <- function(input_df, response_variable)
+#{
+ # input_df %>%
+   # mutate(ASRV_modified= if_else(ASRV==TRUE, "Yes", "No")) -> df_for_modeling
   
   # string works as solution to column argument b/c *IT'S A FORMULA*
-  formula <- paste(
-    response_variable, 
-    "~", 
-    "model + ASRV_modified") %>% as.formula()
+ # formula <- paste(
+  #  response_variable, 
+  #  "~", 
+  #  "model + ASRV_modified") %>% as.formula()
   
   #lm(response_variable ~ model + ASRV_modified, data = df_for_modeling) %>% 
-  lm(formula, data = df_for_modeling) %>%
+ # lm(formula, data = df_for_modeling) %>%
+  #  aov() %>%
+  #  TukeyHSD() -> fitted_model
+  
+  #fitted_model$model %>% 
+  #  as_tibble(rownames = "comparison")
+#}
+
+
+
+
+
+
+# Another way to accomplish the previous function: This function will take a dataframe of branchlengths and a response variable and the output is the lm as a tibble.
+
+linear_model_function_with_curlies<-function(input_branchlength_df, dependent_variable_column)
+{
+  input_branchlength_df %>%
+    mutate(ASRV_modified= if_else(ASRV==TRUE, "Yes", "No")) -> df_for_modeling
+  
+  lm({{dependent_variable_column}} ~ model+ ASRV_modified, data= df_for_modeling) %>%
     aov() %>%
-    TukeyHSD() -> fitted_model
+    TukeyHSD()->fitted_model
   
   fitted_model$model %>% 
     as_tibble(rownames = "comparison")
 }
-
   
 
-plot_compare <- function(input_df, column_to_plot, plot_title, x_label, y_label)
+
+
+
+#This function is useful for plotting any one of the columns in the branchlength dataframes and comparing each model's output to Poisson
+
+plot_compare_function <- function(input_df, column_to_plot, plot_title, x_label, y_label)
 {
   model_order <- c("JTT", "WAG", "LG", "FLU")
   input_df%>%
-    # use "curly-curly"
     select(model,ASRV,id,{{column_to_plot}})%>%
     pivot_wider(names_from = model, values_from = {{column_to_plot}})%>%
     pivot_longer(cols=all_of(model_order), # sshhhhh 
